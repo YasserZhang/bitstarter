537a48a313b5b084f79371aad0475f6bd5b4e61c
Yasser <yaseru2003@gmail.com>
2013-07-12 06:38:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs')
+
+var word = fs.readFileSync('index.html','utf-8')
+
+var string = word.toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
